585293e9f3bdc4ee7f7dff57f8eb6ee6eb5771d4
avishek0769 <avishekadhikary42@gmail.com>
2026-04-09 22:37:14 +0530

    Download dependencies, models and setup the environment for using tensorflow

---
+++ backend/setup.sh
@@ -0,0 +1,10 @@
+nvm install 20
+nvm use 20
+
+pnpm i
+
+cd node_modules/.pnpm/canvas@3.2.3/node_modules/canvas
+npm rebuild --build-addon-from-source
+
+cd node_modules/.pnpm/@tensorflow+tfjs-node@4.22.0_seedrandom@3.0.5/node_modules/@tensorflow/tfjs-node
+npm rebuild --build-addon-from-source
