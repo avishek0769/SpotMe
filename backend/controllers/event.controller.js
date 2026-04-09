ab9d7c961e181eb2d00898a23b1500964384bd52
avishek0769 <avishekadhikary42@gmail.com>
2026-04-09 15:55:04 +0530

    Create the API Routes for Event and createEvent controller

--- backend/controllers/event.controller.js
+++ backend/controllers/event.controller.js
@@ -2,9 +2,30 @@ import Event from "../models/event.model.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
+import mongoose from "mongoose";
 
 const createEvent = asyncHandler(async (req, res) => {
-    
+    const { name, eventDate, accessLevel } = req.body;
+    if (!name || !eventDate) {
+        throw new ApiError(400, "Name and event date are required");
+    }
+    if(accessLevel && !["spot", "browse"].includes(accessLevel)) {
+        throw new ApiError(400, "Invalid access level");
+    }
+
+    const eventId = new mongoose.Types.ObjectId()
+    const event = await Event.create({
+        _id: eventId,
+        name,
+        eventDate,
+        userId: req.user._id,
+        coverImage: "",
+        accessLevel,
+        sharableLink: `${process.env.BASE_URL}/event/${eventId}`,
+        expiresAt: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000) // 30 days from now
+    });
+
+    return res.status(201).json(new ApiResponse(201, event, "Event created successfully"));
 });
 
 export {
--- backend/routers/event.route.js
+++ backend/routers/event.route.js
@@ -5,5 +5,9 @@ import { createEvent } from "../controllers/event.controller.js";
 const eventRouter = Router();
 
 eventRouter.route("/create").post(verifyStrictJWT, createEvent);
+eventRouter.route("/edit").post(verifyStrictJWT);
+eventRouter.route("/delete").delete(verifyStrictJWT);
+eventRouter.route("/signed-url").get(verifyStrictJWT);
+eventRouter.route("/enqueue-batch").patch(verifyStrictJWT);
 
 export default eventRouter;
