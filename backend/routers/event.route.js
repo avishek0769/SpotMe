c0a3697913e8bc7c1790b7e531a8703b8dc43f11
avishek0769 <avishekadhikary42@gmail.com>
2026-04-10 17:46:35 +0530

    Created the left over routes in Event. Controller are left

--- backend/routers/event.route.js
+++ backend/routers/event.route.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { verifyStrictJWT } from "../middlewares/auth.middleware.js";
-import { createEvent, getAllEvents, deleteEvent, editEvent, getSignedUrl, enqueueBatch } from "../controllers/event.controller.js";
+import { verifyStrictJWT, verifyJWT } from "../middlewares/auth.middleware.js";
+import { createEvent, getAllEvents, deleteEvent, editEvent, enqueueBatch } from "../controllers/event.controller.js";
 
 const eventRouter = Router();
 
 eventRouter.route("/create").post(verifyStrictJWT, createEvent);
 eventRouter.route("/list").get(verifyStrictJWT, getAllEvents);
+eventRouter.route("/details/:eventId").get(verifyStrictJWT);
+eventRouter.route("/all-photos/:eventId").get(verifyStrictJWT);
+eventRouter.route("/all-guests/:eventId").get(verifyStrictJWT);
+eventRouter.route("/guest/visit/:eventId").get(verifyJWT);
 eventRouter.route("/edit/:eventId").patch(verifyStrictJWT, editEvent);
 eventRouter.route("/delete/:eventId").delete(verifyStrictJWT, deleteEvent);
 eventRouter.route("/enqueue-batch").post(verifyStrictJWT, enqueueBatch);
