3add024d0b3c47beb0a35382fb7d5b9c7cdf3d13
avishek0769 <avishekadhikary42@gmail.com>
2026-04-11 14:22:21 +0530

    Create all the /signed-url APIs for Photo

--- backend/controllers/photo.controller.js
+++ backend/controllers/photo.controller.js
@@ -1,11 +1,12 @@
 import Photo from "../models/photo.model.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { ApiError } from "../utils/ApiError.js";
 import { createPresignedPost } from "@aws-sdk/s3-presigned-post";
-import { S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 
-const s3Client = new S3Client({
+
+const s3 = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -13,10 +14,10 @@ const s3Client = new S3Client({
     }
 });
 
-const getSignedUrl = asyncHandler(async (req, res) => {
+const getSignedUrlForEvent = asyncHandler(async (req, res) => {
     const { eventId } = req.params;
 
-    const { url, fields } = await createPresignedPost(s3Client, {
+    const { url, fields } = await createPresignedPost(s3, {
         Bucket: process.env.AWS_S3_BUCKET_NAME,
         Key: `event_images/${eventId}/` + "${filename}",
         Conditions: [
@@ -33,6 +34,25 @@ const getSignedUrl = asyncHandler(async (req, res) => {
     ));
 });
 
+const getSignedUrlForSelfie = asyncHandler(async (req, res) => {
+    const { collectionId } = req.params;
+    const { filename } = req.query;
+
+    const getObjectCommand = new GetObjectCommand({
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: `selfies/${collectionId}/${filename}`
+    });
+
+    const signedUrl = await getSignedUrl(s3, getObjectCommand, { expiresIn: 3600 });
+
+    return res.status(200).json(new ApiResponse(
+        200,
+        { url: signedUrl },
+        "Signed URL generated successfully"
+    ));
+})
+
 export {
-    getSignedUrl
+    getSignedUrlForEvent,
+    getSignedUrlForSelfie
 }
