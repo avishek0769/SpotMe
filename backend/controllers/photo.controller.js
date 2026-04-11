7caca0ad8c7e5ed5c26173eabdd5ddb4d85d0929
avishek0769 <avishekadhikary42@gmail.com>
2026-04-11 14:23:35 +0530

    Create the Download photo APIs

--- backend/controllers/photo.controller.js
+++ backend/controllers/photo.controller.js
@@ -4,7 +4,8 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { createPresignedPost } from "@aws-sdk/s3-presigned-post";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
-
+import archiver from "archiver";
+import { QdrantClient } from "@qdrant/js-client-rest"
 
 const s3 = new S3Client({
     region: process.env.AWS_REGION,
@@ -52,7 +53,62 @@ const getSignedUrlForSelfie = asyncHandler(async (req, res) => {
     ));
 })
 
+const downloadSelected = asyncHandler(async (req, res) => {
+    const { fileNames } = req.body;
+    const { eventId } = req.params;
+    const archive = archiver("zip", { zlib: { level: 5 } })
+
+    res.attachment("selected_images.zip");
+    archive.pipe(res)
+
+    for (const fileName of fileNames) {
+        const command = new GetObjectCommand({
+            Bucket: process.env.AWS_S3_BUCKET_NAME,
+            Key: `event_images/${eventId}/${fileName}`
+        })
+        const { Body } = await s3.send(command);
+        archive.append(Body, { name: fileName });
+    }
+
+    archive.finalize();
+});
+
+const downloadAll = asyncHandler(async (req, res) => {
+    const { eventId, collectionId } = req.params;
+
+    let allPhotos;
+    if (collectionId) {
+        allPhotos = await Photo.find({
+            $in: { collectionIds: collectionId },
+            eventId
+        });
+    }
+    else {
+        allPhotos = await Photo.find({ eventId });
+    }
+
+    const allFileKeys = allPhotos.map(photo => photo.url.split("/").pop())
+
+    const archive = archiver("zip", { zlib: { level: 5 } })
+
+    res.attachment("all_images.zip");
+    archive.pipe(res)
+
+    for (const fileName of allFileKeys) {
+        const command = new GetObjectCommand({
+            Bucket: process.env.AWS_S3_BUCKET_NAME,
+            Key: `event_images/${eventId}/${fileName}`
+        })
+        const { Body } = await s3.send(command);
+        archive.append(Body, { name: fileName });
+    }
+
+    archive.finalize();
+})
+
 export {
     getSignedUrlForEvent,
-    getSignedUrlForSelfie
+    getSignedUrlForSelfie,
+    downloadSelected,
+    downloadAll
 }
