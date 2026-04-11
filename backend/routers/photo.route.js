8bfee30738d56c58db8b531812f031cb8a97c8b8
avishek0769 <avishekadhikary42@gmail.com>
2026-04-11 14:17:56 +0530

    Define all the routes in Photo API endpoints

--- backend/routers/photo.route.js
+++ backend/routers/photo.route.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { verifyStrictJWT } from "../middlewares/auth.middleware.js";
-import { getSignedUrl } from "../controllers/photo.controller.js";
+import { getSignedUrlForEvent, getSignedUrlForSelfie, downloadAll, downloadSelected, deletePhoto } from "../controllers/photo.controller.js";
 
 const photoRouter = Router();
 
-photoRouter.route("/signed-url/:eventId").get(verifyStrictJWT, getSignedUrl);
+photoRouter.route("/signed-url/event/:eventId").get(verifyStrictJWT, getSignedUrlForEvent);
+photoRouter.route("/signed-url/selfie/:collectionId").get(verifyStrictJWT, getSignedUrlForSelfie);
+photoRouter.route("/download/selected/:eventId").post(verifyStrictJWT, downloadSelected);
+photoRouter.route("/download/all/event/:eventId").get(verifyStrictJWT, downloadAll);
+photoRouter.route("/download/all/collection/:collectionId/:eventId").get(verifyStrictJWT, downloadAll); // Test
+photoRouter.route("/delete/event/:eventId").delete(verifyStrictJWT, deletePhoto);
+photoRouter.route("/delete/selfie/:collectionId").delete(verifyStrictJWT); // Test
+photoRouter.route("/all-selfies").delete(verifyStrictJWT);
 
 export default photoRouter;
